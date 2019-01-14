86a7734c67831706ca897d09a9e814203c65e27e
Romain Mosser <mosser.romain@gmail.com>
2019-01-14 16:59:00 +0100

    added rspec and capybara gems for testing

--- Gemfile
+++ Gemfile
@@ -17,6 +17,10 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+
+  # Testing
+  gem 'rspec-rails', '~> 3.8'
+  gem 'factory_bot'
 end
 
 group :development do
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,15 +4,4 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-
-  describe 'Post' do
-
-    describe 'Index' do
-      it 'displays all the posts' do
-        expect(Post :index).to eq(@posts = Post.all)
-      end
-    end
-
-  end
-
 end
