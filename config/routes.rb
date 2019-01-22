f5d54a22544d82461281b7a71ecdc7801cba9d1e
Romain Mosser <mosser.romain@gmail.com>
2019-01-22 10:25:41 +0100

    changed the strftime display

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,5 @@ Rails.application.routes.draw do
   get 'posts/edit'
   get 'posts/show'
   get 'posts/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
