bc6c39fe746b3334153169dfebf1f4a3319e9c40
Romain Mosser <mosser.romain@gmail.com>
2019-03-03 21:34:43 +0100

    text and display corrections

--- app/controllers/evenements_controller.rb
+++ app/controllers/evenements_controller.rb
@@ -1,7 +1,7 @@
 class EvenementsController < ApplicationController
   before_action :authenticate_admin!, only: [:new, :create, :edit, :update, :destroy]
   def index
-    @evenements = Evenement.all
+    @evenements = Evenement.order(:date, :time)
   end
 
   def show
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @hebergements = Hebergement.all
-    @evenements = Evenement.all
+    @evenements = Evenement.order(:date, :time)
     @liste_de_mariages = ListeDeMariage.all
     @contacts = Contact.all
   end
